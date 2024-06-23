5bf6d8d86cf4c0f285ea880117843eed7066020e
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-06-23 15:52:21 -0700

    Include load-init-nametable.inc

--- Atlantico.asm
+++ Atlantico.asm
@@ -2,7 +2,7 @@
 .include "IncFiles/header.inc"
 .include "IncFiles/reset.inc"
 .include "IncFiles/utils.inc"
-
+.include "IncFiles/Macros/load-init-nametable.inc"
 
 ;--------------------------------------------------------
 ; ROM-specific constants
@@ -58,34 +58,36 @@ Main:
   jsr LoadPalette                       ; set palette data
   jsr LoadSprites                       ; set sprites (from tiles)
 
-  lda #1                                ; temporarily set current CurNameTable to 1
-  sta CurrNameTable
+  LOAD_INIT_NAMETABLE
 
-InitNameTableLoop:
-      jsr LoadColumnTiles               ; load a new set of column tiles
+;   lda #1                                ; temporarily set current CurNameTable to 1
+;   sta CurrNameTable
 
-    InitAttrBlockLoad:
-      lda #$03                          ; is the column index a multiple of 4?
-      bit SourceColIndex
-      bne :+
-          jsr LoadAttributeBlocks       ; if yes, load a new set of attribute block
-:
-      lda XScroll                       ; increment XScroll by 8 (to load next column on next iteration)
-      clc
-      adc #8
-      sta XScroll
+; InitNameTableLoop:
+;       jsr LoadColumnTiles               ; load a new set of column tiles
 
-      inc SourceColIndex                ; increment source column index
+;     InitAttrBlockLoad:
+;       lda #$03                          ; is the column index a multiple of 4?
+;       bit SourceColIndex
+;       bne :+
+;           jsr LoadAttributeBlocks       ; if yes, load a new set of attribute block
+; :
+;       lda XScroll                       ; increment XScroll by 8 (to load next column on next iteration)
+;       clc
+;       adc #8
+;       sta XScroll
 
-      lda SourceColIndex                ; have all 32 initial columns been loaded?
-      cmp #32
-  bne InitNameTableLoop                 ; if no, perform another iteration
+;       inc SourceColIndex                ; increment source column index
 
-  ; lda #0
-  ; sta XScroll
+;       lda SourceColIndex                ; have all 32 initial columns been loaded?
+;       cmp #32
+;   bne InitNameTableLoop                 ; if no, perform another iteration
 
-  lda #0                                ; revert CurNameTable back to 0
-  sta CurrNameTable
+;   ; lda #0
+;   ; sta XScroll
+
+;   lda #0                                ; revert CurNameTable back to 0
+;   sta CurrNameTable
 
 EnableRendering:
   lda #%10010100                        ; enable NMI interrupts from PPU, set background to use 2nd pattern table, increment PPU_DATA writes by 32
