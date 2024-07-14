f4bbcd3534580c52711357beab7b893ee58d11a6
Chris Phillips <christopher.trevor.phillips@gmail.com>
2024-07-14 13:43:22 -0700

    Add RAM param vars for DrawMetaSprite

--- atlantico.asm
+++ atlantico.asm
@@ -36,7 +36,13 @@ CurrNameTable:        .res 1            ; [$0D] store the current 'starting' Nam
 SourceColIndex:       .res 1            ; [$0E] index of source column
 DestAddr:             .res 2            ; [$0F] address of destination column in PPU memory map
 SourceAddr:           .res 2            ; [$11] address of source column/attribute in ROM
-ActorsArray:          .res MAX_ACTORS * .sizeof(Actor) ; [$13] array of actors
+OAMRAMIndex:          .res 1            ; [$13] index of OAM RAM data
+DrawMetaSprite_XPos:      .res 1        ; [$14] DrawMetaSprite param (X position of metasprite)
+DrawMetaSprite_YPos:      .res 1        ; [$15] DrawMetaSprite param (Y position of metasprite)
+DrawMetaSprite_TileNum:   .res 1        ; [$16] DrawMetaSprite param (starting tile number of metasprite)
+DrawMetaSprite_Attribs:   .res 1        ; [$17] DrawMetaSprite param (attributes of metasprite)
+DrawMetaSprite_TotalTiles:.res 1        ; [$18] DrawMetaSprite param (total # of tiles comprising metasprite)
+ActorsArray:          .res MAX_ACTORS * .sizeof(Actor) ; [$19] array of actors
 
 ;--------------------------------------------------------
 ; PRG-ROM (at $8000)
